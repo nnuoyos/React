78420cc6c56f57a00a74f2fb08fa5ff51bdbec82
nnuoyos <youngadult3104@gmail.com>
2022-09-13 23:09:28 +0900

    test

--- first/src/App.js
+++ first/src/App.js
@@ -3,9 +3,9 @@ import Box from './component/Box';
 function App() {
     return (
         <div>
-            <Box name="강준" num={1} />
-            <Box name="은우" num={2} />
-            <Box name="보검" num={3} />
+            <Box name="서강준" num={1} />
+            <Box name="차은우" num={2} />
+            <Box name="박보검" num={3} />
         </div>
     );
 }
