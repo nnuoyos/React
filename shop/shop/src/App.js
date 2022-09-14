06dd5a227b58a6e14f84ca1de1f85c74c354b9b5
nnuoyos <youngadult3104@gmail.com>
2022-09-14 18:49:36 +0900

    shopping site mark up

--- shop/shop/src/App.js
+++ shop/shop/src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import ProductAll from './page/ProductAll';
 import Login from './page/Login';
 import ProductDetail from './page/ProductDetail';
+import Navbar from './component/Navbar';
 function App() {
   return (
     <div>
-       <Routes>
+      <Navbar/>
+      <Routes> {/* Routes 안의 내용은 변하지만, navbar는 상단 고정을 위해 따로 빼놓는다 */}
         <Route path="/" element={<ProductAll />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/product/:id" element={<ProductDetail/>} />
@@ -18,6 +20,7 @@ function App() {
 export default App;
 
 // 1. 전체 상품 페이지 , 로그인 페이지, 상품 상세 페이지 필요 -done-
+// 1-1. 로고, 네비게이션 바, 로그인영역이 있다 (사라지지 않고 계속 유지 됨)
 // 2. 전체 상품 페이지에서는 전체 상품을 볼 수 있다
 // 3. 로그인 버튼을 누르면 로그인 페이지가 나타난다
 // 4. 상품 디테일을 눌렀으나 로그인이 안되어 있을 경우 로그인 페이지가 먼저 나온다
---
+++ shop/shop/src/component/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import {faSearch} from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+/* 코드의 재생산, 유지보수를 위해 메뉴의 배열을 만들어준다 */
+const menuList = ['여성','Divided','남성','신생아/유아','아동','H&M HOME','Sale','지속가능성']    
+  return (
+    <div>
+        <div className='login-button'>
+            <div>
+                <FontAwesomeIcon icon={faUser} />
+                <div>로그인</div>
+            </div>
+        </div>
+        <div className='nav-section'>
+            <img src='https://logos-world.net/wp-content/uploads/2020/04/HM-Logo.png'></img>
+        </div>
+        <div className='menu-area'>
+            <ul className='menu-list'>
+                {menuList.map((menu)=>(<li>{menu}</li>))}
+            </ul>
+            <div className='nav-search'>
+                <FontAwesomeIcon icon={faSearch} />
+                <input placeholder='제품검색'></input>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
