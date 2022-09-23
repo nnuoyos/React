aa8449fbb67867dc6e254d33c79ae0269bc997a6
nnuoyos <youngadult3104@gmail.com>
2022-09-23 21:27:25 +0900

    에러 해결

--- weather-app/weather-app/src/App.js
+++ weather-app/weather-app/src/App.js
@@ -1,10 +1,12 @@
-
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ClipLoader from 'react-spinners/ClipLoader';
+import { ClipLoader } from 'react-spinners';
 import './App.css';
+
+//API 출처 : "https://openweathermap.org/"
+//부트스트랩 사용
 
 //1. 앱이 실행 되자 마자 현재 위치 기반의 날씨가 보인다
 //2. 지금 현재 도시와 섭씨/화씨 , 날씨 상태 정보가 나온다
@@ -16,54 +18,55 @@ function App() {
     const [weather, setWeather] = useState('');
     const [error, setError] = useState('');
     const [city, setCity] = useState('');
-    const [id, setId] = useState('');
     const [icon, setIcon] = useState('');
     const [loading, setLoading] = useState(false); //기본값은 false, 데이터 fetch일 때만 true로 바꿔주어 스피너 보여준다
     const cities = ['seoul', 'busan', 'jeju', 'new york', 'tokyo'];
     const [query, setQuery] = useState('');
-    const API_KEY = "81523c3aa0ea13bf7e0d71967cd5d5d4";
-    /* 현재 위치의 위도 경도 가져오기 */
-    const getCurrentLocation = () => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            let lat = position.coords.latitude;
-            let lon = position.coords.longitude;
-            //console.log('현재 위도 경도는?', lat, lon);
-            getWeatherByCurrentLocation(lat, lon);
-        });
-    };
+    const API_KEY = '81523c3aa0ea13bf7e0d71967cd5d5d4';
+    
     /* 검색 기능 */
     const search = async (e) => {
-        try{
-            if(e.key === "Enter"){
+        try {
+            if (e.key === 'Enter') {
                 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`)
-                .then(res => res.json())
-                .then(result => {
-                    setWeather(result);
-                    setQuery('');
-                    //console.log("result 확인", result);
-                })
+                    .then((res) => res.json())
+                    .then((result) => {
+                        setWeather(result);
+                        setQuery('');
+                        //console.log("result 확인", result);
+                    });
             }
-        }catch(error){
+        } catch (error) {
             console.log('잡힌 에러는?', error.message);
             errorRender(error.message);
         }
-    }
-    
+    };
+
     const errorRender = (message) => {
         setError(message);
-        /* 화면에 어떻게 렌더링 해야 하는지??? */
-    }
+        // 화면에 어떻게 렌더링 해야 하는지???
+    };
 
     /* 현재 위치의 날씨 정보 */
     const getWeatherByCurrentLocation = async (lat, lon) => {
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-        let response = await fetch(url);
-        let data = await response.json();
-        //console.log('데이터 확인!', data);
-        setWeather(data);
-        setId(data.weather[0].id);
-        setIcon(data.weather[0].icon);
-        setLoading(false); //스피너 종료
+        try {
+            let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+            let response = await fetch(url);
+            let data = await response.json();
+            setWeather(data);
+            setIcon(data.weather[0].icon);
+            setLoading(false);
+        } catch (err) {
+            setError(err.message);
+            setLoading(false);
+        }
+    };
+    /* 현재 위치의 위도 경도 가져오기 */
+    const getCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+            getWeatherByCurrentLocation(latitude, longitude);
+        });
     };
     /* 도시별 날씨 가져오기 */
     const getWeatherByCity = async () => {
@@ -72,19 +75,16 @@ function App() {
             let response = await fetch(url);
             let data = await response.json();
             setWeather(data);
-            setId(data&&data.weather[0].id);
-            setIcon(data&&data.weather[0].icon);
+            setIcon(data && data.weather[0].icon);
+            setLoading(false);
         } catch (error) {
             console.log('잡힌 에러는?', error.message);
             setError(error.message);
-            setLoading(true);
+            setLoading(false);
         }
-        setLoading(false);
     };
 
-    useEffect((city) => {
-        /* useEffect는 componentDidUpdate 역할도 한다 */
-        /* useEffect 는 한 곳에서 정리한다 */
+    useEffect(() => {
         if (city === '') {
             setLoading(true);
             getCurrentLocation();
@@ -110,23 +110,16 @@ function App() {
                 <div className="container">
                     <ClipLoader color="#ff0000" loading={loading} size={100} />
                 </div>
-            ) : (
-                <div className={(typeof weather.main != "undefined")
-                ? ((weather&&weather.main.temp > 26)
-                    ? 'container_warm' : 'container') : 'container'}>
-                    <div className='search_box'>
-                        <input
-                        type="text"
-                        placeholder='Search...'
-                        onChange={e=>setQuery(e.target.value)}
-                        value={query}
-                        onKeyPress={search}
-                        >
-                        </input>
+            ) : !error ? (
+                <div className={typeof weather.main != 'undefined' ? (weather && weather.main.temp > 26 ? 'container_warm' : 'container') : 'container'}>
+                    <div className="search_box">
+                        <input type="text" placeholder="Search..." onChange={(e) => setQuery(e.target.value)} value={query} onKeyPress={search}></input>
                     </div>
-                    <WeatherBox weather={weather} id={id} icon={icon}/> {/* props로 넘기기 */}
+                    <WeatherBox weather={weather} icon={icon} /> {/* props로 넘기기 */}
                     <WeatherButton cities={cities} selectCity={city} handleCityChange={handleCityChange} /> {/* setCity라는 함수를 props로 넘겨준다 */}
                 </div>
+            ) : (
+                error
             )}
         </div>
     );
--- weather-app/weather-app/src/component/WeatherBox.js
+++ weather-app/weather-app/src/component/WeatherBox.js
@@ -1,35 +1,37 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDroplet, faTemperatureHalf } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDroplet, faTemperatureHalf } from '@fortawesome/free-solid-svg-icons';
 
-const WeatherBox = ({weather, icon}) => {
-    /* console.log('웨더 정보 확인', weather); */
+const WeatherBox = ({ weather, icon }) => {
+    console.log('웨더 정보 확인', weather);
     /* 오늘 날짜 데이터 함수 */
     const todayData = () => {
-        const week = ['MON','TUE','WED','THU','FRI','SAT','SUN'];
+        const week = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
         let now = new Date();
-        let todayMonth = (now.getMonth()+1) > 9 ? (now.getMonth()+1) : '0'+(now.getMonth()+1);
+        let todayMonth = now.getMonth() + 1 > 9 ? now.getMonth() + 1 : '0' + (now.getMonth() + 1);
         let todayDate = now.getDate() > 9 ? now.getDate() : '0' + now.getDate();
         let dayOfWeek = week[now.getDay()];
-        return todayMonth + '-' +todayDate +'-'+ dayOfWeek
-    }
+        return todayMonth + '-' + todayDate + '-' + dayOfWeek;
+    };
     return (
-        <div className='weather_box'>
-            <div className='today'>{todayData()}</div>
-            <div className='location_wrap'>
-                <div className="location">{weather&&weather?.name}</div> {/* weather가 참이면 실행 (삼항연산자의 또다른 표기법) */}
+        <div className="weather_box">
+            <div className="today">{todayData()}</div>
+            <div className="location_wrap">
+                <div className="location">{weather?.name}</div> {/* weather가 참이면 실행 (삼항연산자의 또다른 표기법) */}
             </div>
-            <div className='weather_data_text'>
-                <h2 className='s_tem'>
-                    {Math.round(weather && weather?.main.temp)}º
-                </h2>
-                <div className='weather_data_box'>
-                    <h2 className='feels_like'><FontAwesomeIcon icon={faTemperatureHalf} /> feel like {(weather&&weather?.main.feels_like)}º </h2>
-                    <h2 className='humidity'><FontAwesomeIcon className='faDroplet' icon={faDroplet} /> humidity {weather&&weather?.main.humidity}% </h2>
+            <div className="weather_data_text">
+                <h2 className="temp">{Math.round(weather && weather?.main.temp)}º</h2>
+                <div className="weather_data_box">
+                    <h2 className="feels_like">
+                        <FontAwesomeIcon icon={faTemperatureHalf} /> min {weather && weather?.main.temp_min}º / max {weather && weather?.main.temp_max}º{' '}
+                    </h2>
+                    <h2 className="humidity">
+                        <FontAwesomeIcon className="faDroplet" icon={faDroplet} /> humidity {weather && weather?.main.humidity}%{' '}
+                    </h2>
                 </div>
-                <h3 className='weather_description'>{weather&&weather?.weather[0].main}</h3>
-                <h3 className='weather_icon'>
-                <img className="icon" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather icon" />
+                <h3 className="weather_description">{weather && weather?.weather[0].main}</h3>
+                <h3 className="weather_icon">
+                    <img className="icon" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather icon" />
                 </h3>
             </div>
         </div>
