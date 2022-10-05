b3b99cd1c426ce701bb8a2a05b71590483dbe869
nnuoyos <youngadult3104@gmail.com>
2022-10-05 18:20:06 +0900

    리덕스

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,17 +1,29 @@
-import { useState } from 'react';
+/* import { useState } from 'react'; */
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import Box from "./component/Box"
 
 function App() {
-  const [counter, setCounter] = useState(0); 
+/*   const [counter, setCounter] = useState(0); */
+  const dispatch = useDispatch()
+
+  const count = useSelector((state) => state.count);
+
   const increase = () => {
-  setCounter(counter+1);
-  };
+    dispatch({type: "INCREMENT"})
+  }
+  
+  const decrease = () => {
+    dispatch({type: "DECREMENT"})
+  }
+
   return (
     <div>
-      <h1>{counter}</h1>
+      <h1>{count}</h1>
       <button onClick={increase}>증가</button>
+      <button onClick={decrease}>감소</button>
+      <Box/>
     </div>
   );
-}
-
-export default App;
+  }  
+export default App
--- redux/src/redux/reducer/reducer.js
+++ redux/src/redux/reducer/reducer.js
@@ -2,7 +2,20 @@ let initialState = {
     count : 0,
 }
 function reducer(state=initialState, action){
+    console.log("액션 확인!", action);
+    /* if(action.type == "INCREMENT"){
+        return {...state, count: state.count + 1}
+    }
+    return {...state} */
 
+    /* switch문으로 행동지침을 작성한다면 */
+    switch(action.type){
+        case "INCREMENT": 
+            return {...state, count: state.count + 1}
+        case "DECREMENT": 
+            return {...state, count: state.count - 1}
+        default: return {...state}
+    }
 }
 
 export default reducer;
